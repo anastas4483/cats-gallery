80cdea9814f1c4003f027f0d57dbdb4dece80c5b
Anastasia Sorokina <nastasya.sorokina1@gmail.com>
2024-04-07 22:29:24 +0300

    init project

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+const App = () => {
+
+  return (
+    <>First commit</>
+  );
+};
+
+root.render(<App />);
